c2382b20aff689daa6e4ff65f80edf641af3f51c
BastienSkn <sukiennikbastien@gmail.com>
2024-05-25 12:47:30 +0000

    add my_print_revert

---
+++ my_print_revert.c
@@ -0,0 +1,11 @@
+void print_char(char);
+int my_str_len(char*);
+
+void my_print_revert(char *toRevert)
+{
+    int len = my_str_len(toRevert);
+    for (int valeur = len ; valeur >= 0 ; valeur-=1)
+    {
+        print_char(toRevert[valeur]);
+    }
+}
---
+++ my_str_len.c
@@ -0,0 +1,18 @@
+int my_str_len(char* chaine)
+{
+    int len = 0;
+    int x = 0;
+    while (x < 1)
+    {
+        char a = '\0';
+        if (chaine[len] == a)
+        {
+            x = 1;
+        }
+        else
+        {
+            len += 1;
+        }
+    }
+    return len;
+}
