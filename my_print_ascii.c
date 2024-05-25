68678aeb98e37e1e3ff70b996611df7d0185b4f1
BastienSkn <sukiennikbastien@gmail.com>
2024-05-25 10:36:50 +0000

    replace my_print_ascii.c

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -2,7 +2,7 @@ void print_char(char);
 
 void my_print_ascii(void)
 {
-    for (char valeur = 36 ; valeur < 127 ; valeur+=1)
+    for (char valeur = 33 ; valeur < 127 ; valeur+=1)
     {
         print_char(valeur);
     }
